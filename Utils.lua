cc0586ff8ddb6aaa8cb2efb2bb8813c5b458797a
MarkW <m.wissie@gmail.com>
2025-05-25 13:51:00 +0200

    Allow inspecting people outside your party or raid

--- Utils.lua
+++ Utils.lua
@@ -162,6 +162,10 @@ function AddOn:IsItemEquippedInSlot(slot, isInspect)
     local slotID = slot:GetID()
     if isInspect then
         DebugPrint("Inspected unit GUID:", self.db.profile.inspectedUnitGUID)
+        if UnitGUID(InspectFrame.unit) == self.db.profile.inspectedUnitGUID then
+            local itemLink = GetInventoryItemLink(token, slotID)
+            if itemLink then return true, Item:CreateFromItemLink(itemLink) end
+        end
         if IsInRaid() then
             local numRaidMembers = GetNumGroupMembers()
             for i = 1, numRaidMembers do
@@ -193,9 +197,9 @@ function AddOn:IsItemEquippedInSlot(slot, isInspect)
                     if itemLink then
                         return true, Item:CreateFromItemLink(itemLink)
                     end
-                    return false, {}
                 end
             end
+            return false, {}
         end
     else
         local item = Item:CreateFromEquipmentSlot(slot:GetID())
@@ -339,7 +343,7 @@ function AddOn:GetMinMaxItemLevelsFromGear(isInspect)
             local itemLevel = item:GetCurrentItemLevel()
             -- Ignore shirts and tabards
             local isShirtOrTabard = slot == CharacterShirtSlot or slot == CharacterTabardSlot or slot == "InspectShirtSlot" or slot == "InspectTabardSlot"
-            if itemLevel > 0 and not isShirtOrTabard then allItemLevels[slot:GetID()] = itemLevel end
+            if itemLevel > 0 and not isShirtOrTabard then allItemLevels[checkedSlot:GetID()] = itemLevel end
         end
     end
 
